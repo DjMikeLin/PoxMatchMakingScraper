04da70c782d19641460a0779bbecf1014496853f
DjMikeLin <linmichael97@yahoo.com>
2020-02-29 23:58:22 -0500

    working

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 
 (async () => {
   	try{
-	  	const extractAll = async url => {
+	  	const extractAll = async (url, userId) => {
 	  		console.log(url);
 	  		await page.goto(url);//Current page
 
@@ -27,7 +27,7 @@ const fs = require('fs-extra');
 		  			currRowArr.push(currText);
 		  		}
 		  		//adds contents to out.csv. currRowArr can only be length 8.
-		  		await fs.appendFile('out.csv', `"${currRowArr[0]}","${currRowArr[1]}","${currRowArr[2]}","${currRowArr[3]}","${currRowArr[4]}","${currRowArr[5]}","${currRowArr[6]}","${currRowArr[7]}"\n`);
+		  		await fs.appendFile(fileName, `"${currRowArr[0]}","${currRowArr[1]}","${currRowArr[2]}","${currRowArr[3]}","${currRowArr[4]}","${currRowArr[5]}","${currRowArr[6]}","${currRowArr[7]}"\n`);
 		  		currRowArr = [];//resets array;
 		  	}
 		  	//Checks if darkRows or greyRows don't equal 10. If either is true base case is reached
@@ -35,12 +35,19 @@ const fs = require('fs-extra');
 		  		return;
 		  	else{//if base case is not reached go to next page in match history
 			  	const nextPageNumber = parseInt(url.match(/&fb=&p=(\d+)&fb=$/)[1], 10) + 1;
-		  		const nextUrl = `https://www.poxnora.com/account/matchhistory.do?&i=1530499&fb=&p=${nextPageNumber}&fb=`;
-				return await extractAll(nextUrl);
+		  		const nextUrl = `https://www.poxnora.com/account/matchhistory.do?&i=${userId}&fb=&p=${nextPageNumber}&fb=`;
+				return await extractAll(nextUrl, userId);
 	  		}
 	  	};
-	  	//set headers
-	  	await fs.writeFile('out.csv', 'Win,Loss,Type,Ranked,Map,Rating,Date,Duration\n');
+        let user = process.argv[2];
+        const currDate = new Date();
+        const currMonth = currDate.getUTCMonth() < 10 ? '0' + currDate.getUTCMonth() : currDate.getUTCMonth();
+        const currDay = currDate.getDate() < 10 ? '0' + currDate.getDate() : currDate.getDate();
+	  	const currYear = currDate.getUTCFullYear();
+
+        const fileName = `${user}${currMonth}${currDay}${currYear}.csv`;
+        //set headers
+	  	await fs.writeFile(fileName, 'Win,Loss,Type,Ranked,Map,Rating,Date,Duration\n');
 		const browser = await puppeteer.launch({ headless: false });
 	   	const page = await browser.newPage();
 	  	//To view match history, it requires login and a submit button click
@@ -52,9 +59,8 @@ const fs = require('fs-extra');
 	  		page.waitForNavigation(),
 	  		page.click("button[type='submit']")
 	  	]);
-
-        let test = 'ballballer'
-        await page.goto(`https://www.poxnora.com/users/viewprofile.do?u=${test}`);
+      
+        await page.goto(`https://www.poxnora.com/users/viewprofile.do?u=${user}`);
 
 	  	await Promise.all([
 	  		page.waitForNavigation(),
@@ -63,7 +69,7 @@ const fs = require('fs-extra');
 
         let userId = page.url().match(/i=(\d+)&fb=/)[1];
 	  	//Initial call of the recursive function with initial url at page 0
-	  	await extractAll(`https://www.poxnora.com/account/matchhistory.do?&i=${userId}&fb=&p=0&fb=`);
+	  	await extractAll(`https://www.poxnora.com/account/matchhistory.do?&i=${userId}&fb=&p=0&fb=`, userId);
 
 	  	//await page.screenshot({path: 'example.png'});
 	  	await browser.close();
